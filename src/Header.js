19f4f3030c176ac645d25cf1e139fb4313c629a4
Brodie Turner <64994857+broturn@users.noreply.github.com>
2021-02-06 17:29:58 -0600

    Started building out the header

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import MenuIcon from '@material-ui/icons/Menu';
 import "./Header.css";
+import { IconButton } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import { ArrowDropDown } from '@material-ui/icons';
 
 function Header() {
     return (
         <div className="header">
-            <h2>This is the header</h2>
+            <div className="header__left">
+            <IconButton>
+            <MenuIcon />
+            </IconButton>
+            <img src="https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png" alt=""/>
+            </div>
+.            <div className="header__middle">
+
+            </div>
+            <div className="header__right">
+                <SearchIcon />
+                <input placeholder="Search mail" type="text" />
+                <ArrowDropDown className="header__inputCaret" />
+
+            </div>
         </div>
     )
 }

faab35ec7e040e27ad0ca1dd76240ffd067a02b1
Brodie Turner <64994857+broturn@users.noreply.github.com>
2021-02-06 16:46:54 -0600

    installed material ui/core and icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>hi</h1>
+    <Header />
+      <h1>What up yo</h1>
+      
     </div>
   );
 }

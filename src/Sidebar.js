a346f444e2ce8a6f0c3f1d6bc1d6099f017e616f
Brodie Turner <64994857+broturn@users.noreply.github.com>
2021-02-15 17:35:20 -0600

    styled the sidebar component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React from "react";
 import "./Sidebar.css";
+import { Button } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import InboxIcon from "@material-ui/icons/Inbox";
+import SidebarOption from "./SidebarOption";
 
 function Sidebar() {
-    return (
-        <div className="sidebar">
-            
-        </div>
-    )
+  return (
+    <div className="sidebar">
+      <Button startIcon={<AddIcon fontSize="large" />}className="sidebar__compose">Compose</Button>
+      <SidebarOption Icon={InboxIcon} title="Inbox" number={54} />
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;

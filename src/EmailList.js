f1680c502db0e871cdb1e5571524c2f8607d228f
Brodie Turner <64994857+broturn@users.noreply.github.com>
2021-02-16 13:59:46 -0600

    Built EmailRow component and css file

--- src/EmailList.js
+++ src/EmailList.js
@@ -3,6 +3,7 @@ import { Checkbox, IconButton } from "@material-ui/core";
 import "./EmailList.css";
 import { ArrowDropDown, ChevronLeft, ChevronRight, Inbox, KeyboardHide, LocalOffer, MoreVert, People, Redo, Settings } from '@material-ui/icons';
 import Section from './Section';
+import EmailRow from './EmailRow';
 
 function EmailList() {
     return (
@@ -39,6 +40,10 @@ function EmailList() {
                 <Section Icon={Inbox} title="Primary" color="red" selected />
                 <Section Icon={People} title="Social" color="#1A73E8" />
                 <Section Icon={LocalOffer} title="Promotions" color="green" />
+            </div>
+
+            <div className="emailList__List">
+                <EmailRow />
             </div>
         </div>
     )

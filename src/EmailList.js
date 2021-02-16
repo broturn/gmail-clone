8ca0e9be382f2f687d486458aa54e0b09c5f1d32
Brodie Turner <64994857+broturn@users.noreply.github.com>
2021-02-16 13:07:00 -0600

    created EmailList and Mail components and css files

---
+++ src/EmailList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./EmailList.css";
+
+function EmailList() {
+    return (
+        <div className="emailList">
+            <div className="emailList__settings">
+                
+            </div>
+        </div>
+    )
+}
+
+export default EmailList;

a623613ad8f9fb401f43ce0910c31e70195bc069
Brodie Turner <64994857+broturn@users.noreply.github.com>
2021-02-16 13:17:59 -0600

    Started building out emailList

--- src/EmailList.js
+++ src/EmailList.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import { Checkbox, IconButton } from "@material-ui/core";
 import "./EmailList.css";
+import { ArrowDropDown, ChevronLeft, ChevronRight, KeyboardHide, MoreVert, Redo, Settings } from '@material-ui/icons';
 
 function EmailList() {
     return (
         <div className="emailList">
             <div className="emailList__settings">
+                <div className="emailList__settingsLeft">
+                    <Checkbox />
+                    <IconButton>
+                        <ArrowDropDown/>
+                    </IconButton>
+                    <IconButton>
+                        <Redo />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                </div>
+                <div className="emailList__settingRight">
+                <IconButton>
+                        <ChevronLeft />
+                    </IconButton>
+                    <IconButton>
+                        <ChevronRight />
+                    </IconButton>
+                    <IconButton>
+                        <KeyboardHide />
+                    </IconButton>
+                    <IconButton>
+                        <Settings />
+                    </IconButton>
+                </div>
+            </div>
+            <div className="emailList__sections">
                 
             </div>
         </div>

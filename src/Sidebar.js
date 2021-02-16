e90fa2eb720a7792c2ce22111d269a8c1216f431
Brodie Turner <64994857+broturn@users.noreply.github.com>
2021-02-15 18:15:04 -0600

    added a footer to sidebar and styled buttons that are in it.

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Sidebar.css";
-import { Button } from "@material-ui/core";
+import { Button, IconButton } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import InboxIcon from "@material-ui/icons/Inbox";
 import StarIcon from "@material-ui/icons/Star";
@@ -9,6 +9,10 @@ import LabelImportantIcon from "@material-ui/icons/LabelImportant";
 import NearMeIcon from "@material-ui/icons/NearMe";
 import NoteIcon from "@material-ui/icons/Note";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import PersonIcon from "@material-ui/icons/Person";
+import DuoIcon from "@material-ui/icons/Duo";
+import PhoneIcon from "@material-ui/icons/Phone";
+
 
 import SidebarOption from "./SidebarOption";
 
@@ -23,6 +27,21 @@ function Sidebar() {
       <SidebarOption Icon={NearMeIcon} title="Sent" number={54} />
       <SidebarOption Icon={NoteIcon} title="Drafts" number={54} />
       <SidebarOption Icon={ExpandMoreIcon} title="More" number={54} />
+
+      <div className="sidebar__footer">
+        <div className="sidebar__footerIcons">
+          <IconButton>
+            <PersonIcon />
+          </IconButton>
+          <IconButton>
+            <DuoIcon />
+          </IconButton>
+          <IconButton>
+            <PhoneIcon />
+          </IconButton>
+
+        </div>
+      </div>
     </div>
   );
 }

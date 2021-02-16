24357b0674a33230eee1c3ebfa360d8d03f74f2d
Brodie Turner <64994857+broturn@users.noreply.github.com>
2021-02-16 13:30:13 -0600

    Styled emailList and build section component and css file.

--- src/EmailList.js
+++ src/EmailList.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Checkbox, IconButton } from "@material-ui/core";
 import "./EmailList.css";
-import { ArrowDropDown, ChevronLeft, ChevronRight, KeyboardHide, MoreVert, Redo, Settings } from '@material-ui/icons';
+import { ArrowDropDown, ChevronLeft, ChevronRight, Inbox, KeyboardHide, LocalOffer, MoreVert, People, Redo, Settings } from '@material-ui/icons';
+import Section from './Section';
 
 function EmailList() {
     return (
@@ -35,7 +36,9 @@ function EmailList() {
                 </div>
             </div>
             <div className="emailList__sections">
-                
+                <Section Icon={Inbox} title="Primary" color="red" selected />
+                <Section Icon={People} title="Social" color="#1A73E8" />
+                <Section Icon={LocalOffer} title="Promotions" color="green" />
             </div>
         </div>
     )
--- src/Header.js
+++ src/Header.js
@@ -14,7 +14,7 @@ function Header() {
             </IconButton>
             <img src="https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png" alt=""/>
             </div>
-.            <div className="header__middle">
+            <div className="header__middle">
 <SearchIcon />
                 <input placeholder="Search mail" type="text" />
                 <ArrowDropDown className="header__inputCaret" />

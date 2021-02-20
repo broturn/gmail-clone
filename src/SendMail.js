0d3b57fd2506de44cc549da80257e63c0d35168a
Brodie Turner <64994857+broturn@users.noreply.github.com>
2021-02-19 23:05:21 -0600

    built out SendMail Component

--- src/SendMail.js
+++ src/SendMail.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
+import "./SendMail.css";
+import { Close } from '@material-ui/icons';
+import { Button } from '@material-ui/core';
 
 function SendMail() {
     return (
         <div className="sendMail">
-            
+            <div className="sendMail__header">
+                <h3>New Message</h3>
+                <Close className="sendMail__close" />
+            </div>
+
+            <form>
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+
+                <div className="sendMail__options">
+                    <Button>Send</Button>
+                </div>
+            </form>
         </div>
     )
 }
 
-export default SendMail
+export default SendMail;

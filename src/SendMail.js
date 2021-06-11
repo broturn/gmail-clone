37b604c2f87c4c8050b8b1cb1b3738cf42c2c0b6
Brodie Turner <64994857+broturn@users.noreply.github.com>
2021-06-10 21:34:24 -0500

    started using react-hook-form

--- src/SendMail.js
+++ src/SendMail.js
@@ -2,8 +2,14 @@ import React from 'react';
 import "./SendMail.css";
 import { Close } from '@material-ui/icons';
 import { Button } from '@material-ui/core';
+import { useForm } from 'react-hook-form';
 
 function SendMail() {
+    const { register, handleSubmit, watch, errors } = useForm();
+
+    const onSubmit= (data) => {
+        console.log(data);
+    }
     return (
         <div className="sendMail">
             <div className="sendMail__header">
@@ -11,10 +17,13 @@ function SendMail() {
                 <Close className="sendMail__close" />
             </div>
 
-            <form>
-                <input placeholder= 'To' type="text" />
-                <input placeholder="Subject"  type="text" />
-                <input placeholder="Message..."  type="text" className="sendMail__message" />
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input name='to' placeholder= 'To' type="text" ref={register({ required: true })} />
+                {errors.to && <p className="sendMail__error">To is Required!</p>}
+                <input name='subject' placeholder="Subject"  type="text" ref={register({ required: true })} />
+                {errors.subject && <p className="sendMail__error">Subject is Required!</p>}
+                <input name='message'  placeholder="Message..."  type="text" className="sendMail__message" ref={register({ required: true })} />
+                {errors.message && <p className="sendMail__error">Message is Required!</p>}
 
                 <div className="sendMail__options">
                     <Button className="sendMail__send" variant="contained" color="primary" type="submit">Send</Button>
